4714ac04ef7f7a4c9e396a9287557bc7e816b214
rawaldeep singh <rawaldeepsingh@rawaldeeps-MacBook-Pro.local>
2023-05-17 04:27:28 +0200

    update

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 // Imports dependencies and set up http server
 const
-  request = require('request'),
-  express = require('express'),
-  { urlencoded, json } = require('body-parser'),
-  app = express();
+    request = require('request'),
+    express = require('express'),
+    { urlencoded, json } = require('body-parser'),
+    app = express();
 
 // Parse application/x-www-form-urlencoded
 app.use(urlencoded({ extended: true }));
@@ -18,135 +18,151 @@ app.use(json());
 
 // Respond with 'Hello World' when a GET request is made to the homepage
 app.get('/', function (_req, res) {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    // Your verify token. Should be a random string.
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
 
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
 
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
     }
   }
 });
 
 // Creates the endpoint for your webhook
 app.post('/webhook', (req, res) => {
-  let body = req.body;
+    let body = req.body;
 
-  // Checks if this is an event from a page subscription
-  if (body.object === 'page') {
+    // Checks if this is an event from a page subscription
+    if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
 
-      // Gets the body of the webhook event
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
+            // Gets the body of the webhook event
+            let webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
 
-      // Get the sender PSID
-      let senderPsid = webhookEvent.sender.id;
-      console.log('Sender PSID: ' + senderPsid);
+            if (webhookEvent.optin) {
+                // Handle opt-in event
+                sendOptinMessage(senderId);
+            }
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhookEvent.message) {
-        handleMessage(senderPsid, webhookEvent.message);
-      } else if (webhookEvent.postback) {
-        handlePostback(senderPsid, webhookEvent.postback);
-      }
-    });
+            // Get the sender PSID
+            let senderPsid = webhookEvent.sender.id;
+            console.log('Sender PSID: ' + senderPsid);
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhookEvent.message) {
+                handleMessage(senderPsid, webhookEvent.message);
+            } else if (webhookEvent.postback) {
+                handlePostback(senderPsid, webhookEvent.postback);
+            }
+        });
 
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
 // Handles messages events
 function handleMessage(senderPsid, receivedMessage) {
-  let response;
+    let response;
 
-  // Checks if the message contains text
-  if (receivedMessage.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of your request to the Send API
-    response = {
-      'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
-    };
-  } else if (receivedMessage.attachments) {
+    // Checks if the message contains text
+    if (receivedMessage.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of your request to the Send API
+        response = {
+            'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+        };
+    } else if (receivedMessage.attachments) {
 
-    // Get the URL of the message attachment
-    let attachmentUrl = receivedMessage.attachments[0].payload.url;
-    response = {
-      'attachment': {
-        'type': 'template',
-        'payload': {
-          'template_type': 'generic',
-          'elements': [{
-            'title': 'Is this the right picture?',
-            'subtitle': 'Tap a button to answer.',
-            'image_url': attachmentUrl,
-            'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
-              }
-            ],
-          }]
-        }
-      }
-    };
-  }
+        // Get the URL of the message attachment
+        let attachmentUrl = receivedMessage.attachments[0].payload.url;
+        response = {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [{
+                        'title': 'Is this the right picture?',
+                        'subtitle': 'Tap a button to answer.',
+                        'image_url': attachmentUrl,
+                        'buttons': [
+                            {
+                                'type': 'postback',
+                                'title': 'Yes!',
+                                'payload': 'yes',
+                            },
+                            {
+                                'type': 'postback',
+                                'title': 'No!',
+                                'payload': 'no',
+                            }
+                        ],
+                    }]
+                }
+            }
+        };
+    }
 
-  // Send the response message
-  callSendAPI(senderPsid, response);
+    // Send the response message
+    callSendAPI(senderPsid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(senderPsid, receivedPostback) {
-  let response;
+    let response;
 
-  // Get the payload for the postback
-  let payload = receivedPostback.payload;
+    // Get the payload for the postback
+    let payload = receivedPostback.payload;
 
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { 'text': 'Thanks!' };
-  } else if (payload === 'no') {
-    response = { 'text': 'Oops, try sending another image.' };
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(senderPsid, response);
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { 'text': 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { 'text': 'Oops, try sending another image.' };
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(senderPsid, response);
 }
+
+
+function sendOptinMessage(senderPsid){
+    response = {
+        'text': `Hi, I am Cris, Home Credit's virtual chat assistant. Before we proceed, here's a friendly reminder: Register your SIM card ASAP! Deadline has been extended until July 25, 2023. If you fail to register, you will lose your mobile number, all remaining load (prepaid subscribers), and access to your mobile payments and transactions. This is in accordance to R.A. 11934 SIM Registration Act.`
+    };
+
+    // Send the response message
+    callSendAPI(senderPsid, response);
+}
+
 
 // Sends response messages via the Send API
 function callSendAPI(senderPsid, response) {
@@ -179,5 +195,5 @@ function callSendAPI(senderPsid, response) {
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
