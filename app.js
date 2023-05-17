f1cfd03c5bdd1ea1663338473b36fdb459e5afdc
rawaldeep singh <rawaldeepsingh@rawaldeeps-MacBook-Pro.local>
2023-05-17 04:59:56 +0200

    update

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ function handlePostback(senderPsid, receivedPostback) {
 
 
 function sendOptinMessage(senderPsid){
-    response = {
+    let response = {
         'text': `Hi, I am Cris, Home Credit's virtual chat assistant. Before we proceed, here's a friendly reminder: Register your SIM card ASAP! Deadline has been extended until July 25, 2023. If you fail to register, you will lose your mobile number, all remaining load (prepaid subscribers), and access to your mobile payments and transactions. This is in accordance to R.A. 11934 SIM Registration Act.`
     };
 
